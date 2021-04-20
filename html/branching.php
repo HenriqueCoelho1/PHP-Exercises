ee7293b193a0e0bd7406d6823e89783077ee3bf3
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-19 21:54:45 -0300

    Branching

---
+++ html/branching.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$Names = array("Pedro", "Enzo", "Rafael", "Thiago", "Joao", "Claypson", "Rodrigo", "Cesar");
+// $namesLength = count($Names);
+$namesLength = sizeof($Names);
+for($i = 0; $i <= 7; $i++){
+    if($Names[$i] == "Pedro"){
+        continue;
+    }
+    echo $Names[$i]. "<br />";
+
+}
+?>
+    
+</body>
+</html>
