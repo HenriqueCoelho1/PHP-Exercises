09e4ae78339cbaaa3d97cf9e632b6e2fd015ae8e
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-19 22:35:44 -0300

    Re-usability

---
+++ html/functionFile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function Welcome(){
+    echo "Welcome <br />";
+
+}
+
+
+function additionUsingParameter($x, $v){
+    $Result = $x + $v;
+    echo "Addition of x: {$x} and v: {$v} is {$Result} <br />";
+
+}
+
+
+
+?>
+    
+</body>
+</html>
