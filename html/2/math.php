e84892cee5616d30dd3dd9ff53356e30aba84891
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-29 21:03:30 -0300

    Maths

---
+++ html/2/math.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo 45 + 145;
+echo "<br />";
+echo 145 - 45;
+echo "<br />";
+echo 4 * 145;
+
+$number1 = 125;
+$number2 = 125;
+echo "<br />";
+echo $number1 * $number2;
+?>
+    
+</body>
+</html>
