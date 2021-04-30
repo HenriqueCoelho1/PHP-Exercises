e9b13c0e2ce2193ed7a9ec44b1a47d4d80028b48
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-30 18:29:58 -0300

    Switch Statements

---
+++ html/2/control-structures/switch_statements.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$number = 0;
+
+switch($number){
+    case 34:
+        echo "Is it 34 <br />";
+        break;
+    case 37:
+        echo "Is it 37 <br />";
+        break;
+    case 32:
+        echo "Is it 32 <br />";
+        break;
+    case 31:
+        echo "Is it 31 <br />";
+        break;
+    default:
+        echo "It is not a number";
+        break;
+
+
+}
+?>
+    
+</body>
+</html>
