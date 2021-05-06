11b9b0b2a28f1ee78a8b832f4bc378d1bb17c032
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-05 22:03:34 -0300

    Receiving Post Data From Check

--- html/2/database-sql/login.php
+++ html/2/database-sql/login.php
@@ -1,3 +1,10 @@
+<?php
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,18 +20,31 @@
         <div class="container">
             <div class='columns is-centered'>
                 <div class="is-5-tablet is-4-desktop is-3-widescreen">
-                    <form action="" method="post">
+                    <form action="login.php" method="post">
                         <h3 class="title is-3">Login</h3>
                         <div class='field p-1'>
                             <label class='label'>Name: </label>
                             <div class="control">
-                                <input class='input primary' type='text' placeholder='Put Your Name Here'/>
+                                <input class='input primary' 
+                                type='text' 
+                                placeholder='Put Your Name Here'
+                                name='name'
+                                value='name'/>
                             </div>
                         </div>
                         <div class='field p-1'>
                             <label class='label'>Password: </label>
                             <div class="control">
-                                <input class='input primary' type='password' placeholder='Put Your Password Here'/>
+                                <input class='input primary' 
+                                type='password' 
+                                placeholder='Put Your Password Here' 
+                                name='password'
+                                value='password'/>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <div class="button is-primary" name="submit" value="submit">Submit</div>
                             </div>
                         </div>
                     </form>
