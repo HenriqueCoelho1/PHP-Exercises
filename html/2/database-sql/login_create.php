12507c5766e2022fc357745b2f238147433f758d
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-07 19:23:57 -0300

    Fixing and Refactoring all

--- html/2/database-sql/login_create.php
+++ html/2/database-sql/login_create.php
@@ -4,17 +4,8 @@ if(isset($_POST['submit'])){
     createTable();
 }
 ?>
+<?php include "include/header.php";?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-    <title>Document</title>
-</head>
-<body>
     <section class="hero has-background-white-ter is-fullheight">
         <div class="hero-body">
             <div class="container">
@@ -49,9 +40,4 @@ if(isset($_POST['submit'])){
             </div>
         
         </div>
-
-    </section>
-
-    
-</body>
-</html>
+        <?php include "include/footer.php";?>
