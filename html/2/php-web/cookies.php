785765a28ca08c76e7148839cc5a6339845d0482
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-09 23:42:52 -0300

    Cookies

---
+++ html/2/php-web/cookies.php
@@ -0,0 +1,19 @@
+<?php
+$name = "Some name";
+$value = 100;
+$expiration = time() + (60 * 60 * 24 * 7); //the 60 is equal to seconds the seconds 60 is equal to minutes, 24 is for hours, and 7 is for days;
+setcookie($name, $value, $expiration);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
